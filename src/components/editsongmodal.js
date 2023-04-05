28fab79061329ee0108ff33ff17eef63892b2a9e
Jonathan Loescher <jonathan@jloescher.com>
2023-04-05 00:31:36 -0400

    Working on Modals

--- src/components/editsongmodal.js
+++ src/components/editsongmodal.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import Modal from './modal';
 
-const EditSongModal = ({ song, setSong, handleClose }) => {
+const EditSongModal = ({ song, setSong, handleClose, isOpen, onClose }) => {
   const [title, setTitle] = useState(song.title);
   const [artist, setArtist] = useState(song.artist);
   const [album, setAlbum] = useState(song.album);
@@ -11,45 +12,47 @@ const EditSongModal = ({ song, setSong, handleClose }) => {
   };
 
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <span className="close" onClick={handleClose}>&times;</span>
-        <h2>Edit Song</h2>
-        <form onSubmit={handleSave}>
-          <div>
-            <label htmlFor="title">Title:</label>
-            <input
-              type="text"
-              id="title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="artist">Artist:</label>
-            <input
-              type="text"
-              id="artist"
-              value={artist}
-              onChange={(e) => setArtist(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="album">Album:</label>
-            <input
-              type="text"
-              id="album"
-              value={album}
-              onChange={(e) => setAlbum(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit">Save</button>
-        </form>
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={handleClose}>&times;</span>
+          <h2>Edit Song</h2>
+          <form onSubmit={handleSave}>
+            <div>
+              <label htmlFor="title">Title:</label>
+              <input
+                type="text"
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="artist">Artist:</label>
+              <input
+                type="text"
+                id="artist"
+                value={artist}
+                onChange={(e) => setArtist(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="album">Album:</label>
+              <input
+                type="text"
+                id="album"
+                value={album}
+                onChange={(e) => setAlbum(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit">Save</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </Modal>
   );
 };
 
--- src/components/modal.js
+++ src/components/modal.js
@@ -5,32 +5,35 @@ const Modal = ({ isOpen, handleClose, children }) => {
 
   return (
     <div className={showHideClassName}>
-      <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div className="flex items-center justify-center min-h-screen px-4">
         <div className="fixed inset-0 transition-opacity" aria-hidden="true">
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
-        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <div className="sm:flex sm:items-start">
-              {children}
+        <div className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all max-w-md w-full">
+          <div className="bg-white px-4 py-5 sm:px-6">
+            <div className="flex items-start justify-between">
+              <h3 className="text-lg font-medium text-gray-900">{children.header}</h3>
+              <button
+                type="button"
+                className="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150"
+                onClick={handleClose}
+              >
+                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
             </div>
           </div>
-          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <button
-              type="button"
-              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
-              onClick={handleClose}
-            >
-              Add
-            </button>
-            <button
-              type="button"
-              className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-              onClick={handleClose}
-            >
-              Cancel
-            </button>
+          <div className="bg-gray-50 px-4 py-5 sm:p-6">
+            {children.body}
+          </div>
+          <div className="bg-gray-50 px-4 py-4 sm:px-6 sm:flex sm:flex-row-reverse">
+            {children.footer}
           </div>
         </div>
       </div>
